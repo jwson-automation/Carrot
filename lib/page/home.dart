02c226543da88339c5906c1f66d931fcaf61ecd0
jwson-automation <sonjungwoo9@gmail.com>
2022-09-06 20:20:29 +0900

    update

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,9 +1,7 @@
-import 'dart:html';
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,6 +12,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String, String>> datas = [];
+  int _currentPageIndex;
 
   @override
   void initState() {
@@ -123,6 +122,12 @@ class _HomeState extends State<Home> {
     );
   }
 
+  final oCcy = new NumberFormat("#,###", "ko_KR");
+
+  String calcStringToWon(String priceString) {
+    return "${oCcy.format(int.parse(priceString))}원";
+  }
+
   Widget _bodyWidget() {
     return ListView.separated(
       padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -147,19 +152,28 @@ class _HomeState extends State<Home> {
                 child: Container(
                   height: 100,
                   child: Container(
-                    color: Colors.grey,
+                    color: Colors.black,
                     child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             datas[index]["title"]!,
                             overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 15,
-                            ),
+                            style: TextStyle(fontSize: 15, color: Colors.white),
                           ),
-                          Text(datas[index]["location"]!),
-                          Text(datas[index]["price"]!),
+                          Text(
+                            datas[index]["location"]!,
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white.withOpacity(0.5)),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            calcStringToWon(datas[index]["price"]!),
+                            style: TextStyle(fontSize: 12, color: Colors.white),
+                          ),
                           Expanded(
                             child: Container(
                               // color: Colors.red,
@@ -173,7 +187,10 @@ class _HomeState extends State<Home> {
                                     height: 13,
                                   ),
                                   SizedBox(width: 5),
-                                  Text(datas[index]["likes"]!),
+                                  Text(
+                                    datas[index]["likes"]!,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
                                 ],
                               ),
                             ),
@@ -193,11 +210,39 @@ class _HomeState extends State<Home> {
     );
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(
+      String iconName, String label) {
+    return BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          "assets/svg/${iconName}_off.svg",
+          width: 22,
+        ),
+        label: label);
+  }
+
+  Widget _bottomNavigationBarwidget() {
+    return BottomNavigationBar(
+      onTap: (int index) {
+        print(index);
+      },
+      currentIndex: ,
+      items: [
+        _bottomNavigationBarItem("home", '홈'),
+        _bottomNavigationBarItem("notes", '암흑생활'),
+        _bottomNavigationBarItem("location", '내 근처'),
+        _bottomNavigationBarItem("chat", '채팅'),
+        _bottomNavigationBarItem("user", '나의 암흑'),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: _appbarWidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bottomNavigationBarwidget(),
       // bottomNavigationBar: Container(color: Colors.black),
     );
   }
